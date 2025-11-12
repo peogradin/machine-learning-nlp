1a509b423bcab85837f359e4ef242c1d7d9cf2c8
Marten <janmarte@chalmers.se>
2025-11-12 13:46:58 +0100

    transformer layers and generation

--- a2/A2.py
+++ a2/A2.py
@@ -1,8 +1,8 @@
 
 import torch
-from torch import nn
+from torch import nn, RMSNorm
 from transformers import PreTrainedModel, PretrainedConfig
-
+from torch.distributions.categorical import Categorical
 
 class A2ModelConfig(PretrainedConfig):
     """Configuration object that stores hyperparameters that define the Transformer language model."""
@@ -27,10 +27,29 @@ class A2MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
         assert(config.hidden_act == 'silu')
-        # TODO: initalize components here
+
+        self.fc1 = nn.Linear(config.hidden_size, config.intermediate_size * 2, bias=False)
+        self.fc2 = nn.Linear(config.intermediate_size, config.hidden_size, bias=False)
+        self.silu = nn.SiLU()
 
     def forward(self, hidden_states):
-        ...
+        hidden_states = self.fc1(hidden_states)
+        hidden_states_1, hidden_states_2 = hidden_states.chunk(2, dim=-1)
+        # SwiLU activation on one half and element-wise multiply with the other half
+        hidden_states = self.silu(hidden_states_1) * hidden_states_2
+        # Final linear layer
+        hidden_states = self.fc2(hidden_states)
+        return hidden_states
+    
+mlp = A2MLP(A2ModelConfig(
+    vocab_size=1000,
+    hidden_size=100,
+    intermediate_size=200,
+))
+# some 3-dimensional tensor where the last dimension has the same size as hidden_size
+x = torch.randn(2, 10, 100)
+output = mlp(x)
+print(output.shape)  # should be the same shape (2, 10, 100)
 
 # This is optional, since you can use PyTorch's RMSNorm.
 class A2RMSNorm(nn.Module):
@@ -60,10 +79,17 @@ class A2DecoderLayer(nn.Module):
     """A complete Transformer decoder layer."""
     def __init__(self, config):
         super().__init__()
-        # TODO: set up attention, MLP, and normalizers here.
+        self.attention = A2Attention(config)
+        self.mlp = A2MLP(config)
+        self.norm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
 
     def forward(self, hidden_states, rope_rotations):
-        ...
+        att = self.attention(hidden_states, rope_rotations)
+        # Normalization and residual connection from input
+        att = self.norm(att) + hidden_states
+        mlp = self.mlp(att)
+        # Residual connection from attention output
+        return  self.norm(mlp) + att
 
 
 class A2Transformer(PreTrainedModel):
@@ -75,8 +101,11 @@ class A2Transformer(PreTrainedModel):
         super().__init__(config)
 
         self.rotary_emb = A2RotaryEmbedding(config)
-        # TODO: Set up the other components here.
-        # TODO: put all transformer decoder layers in a ModuleList.
+        # transformer decoder layers in a ModuleList for proper gradients.
+        self.modules = nn.ModuleList([A2DecoderLayer(config) for _ in range(config.num_hidden_layers)])
+        self.embedding = nn.Embedding(config.vocab_size, config.hidden_size)
+        self.norm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.unembedding = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
 
         # This line should be called after you have set up all components.
         self.post_init()
@@ -85,9 +114,13 @@ class A2Transformer(PreTrainedModel):
     def forward(self, input_ids):
         rope_rotations = self.rotary_emb(input_ids) # pass this to all the transformer decoder layers
 
-        # TODO: Call embedding, transformer decoder layers, last normalizer, and unembedding.
-        ...
-
+        # Call embedding, transformer decoder layers, last normalizer, and unembedding.
+        hidden_states = self.embedding(input_ids)
+        for layer in self.modules:
+            hidden_states = layer(hidden_states, rope_rotations)
+        hidden_states = self.norm(hidden_states)
+        logits = self.unembedding(hidden_states)
+        return logits
 
 #### RoPE implementation (copied and simplified from HuggingFace). ####
 
@@ -135,3 +168,23 @@ class A2RotaryEmbedding(nn.Module):
             cos = emb.cos()
             sin = emb.sin()
             return cos, sin
+
+def generate(model: A2Transformer, prompt: str, tokenizer, max_length: int = 50, topk: int = 5, temperature: float = 1.0):
+    model.eval()
+    input_ids = tokenizer(prompt, return_tensors="pt").to(model.device)
+    generated = input_ids
+    with torch.no_grad():
+        for _ in range(max_length):
+            logits = model(generated)[:, -1, :]
+            if topk is not None:
+               # take the top-k tokens only
+               logits, _ = torch.topk(logits, topk, dim=-1)
+             
+            distr = Categorical(logits=logits / temperature)
+            next_token = distr.sample().unsqueeze(0)
+            generated = torch.cat((generated, next_token), dim=-1)
+            if next_token == tokenizer.eos_token_id:
+                break
+    # convert ids to text 
+    text = [tokenizer.inv_vocabulary[id] for id in generated[0].cpu().numpy()]
+    return text
