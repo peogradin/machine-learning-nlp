f80dfe6617c60fac1709331994b53d63332bf1df
Marten <janmarte@chalmers.se>
2025-12-03 14:58:15 +0100

    basic A5 model + splitting

---
+++ a5/a5.py
@@ -0,0 +1,45 @@
+# %%
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import pandas as pd
+from transformers import pipeline
+
+# %%
+tmp_data = pd.read_json("ori_pqal.json").T
+# some labels have been defined as "maybe", only keep the yes/no answers
+tmp_data = tmp_data[tmp_data.final_decision.isin(["yes", "no"])]
+
+documents = pd.DataFrame({"abstract": tmp_data.apply(lambda row: (" ").join(row.CONTEXTS+[row.LONG_ANSWER]), axis=1),
+             "year": tmp_data.YEAR})
+questions = pd.DataFrame({"question": tmp_data.QUESTION,
+             "year": tmp_data.YEAR,
+             "gold_label": tmp_data.final_decision,
+             "gold_context": tmp_data.LONG_ANSWER,
+             "gold_document_id": documents.index})
+
+questions.iloc[0].question
+
+# %%
+documents.iloc[0].abstract
+# %%
+
+import os
+os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = "1"
+
+pipe = pipeline("text-generation", model="HuggingFaceTB/SmolLM2-1.7B-Instruct")
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+pipe(messages)
+# %%
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,  # chunk size (characters)
+    chunk_overlap=200,  # chunk overlap (characters)
+    add_start_index=True,  # track index in original document
+)
+
+metadatas = [{"id": idx} for idx in documents.index]
+texts = text_splitter.create_documents(texts=documents.abstract.tolist(), metadatas=metadatas)
+
+splits = text_splitter.split_documents(texts)
+
+# %%
